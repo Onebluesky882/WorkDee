c34eea98959229a4095e241ed24702f381298f2f
Onebluesky882 <onebluesky882@outlook.com>
2025-11-02 00:44:32 +0700

    build : web structure

---
+++ lib/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter_projects/features/auth/view/login_page.dart';
+// import 'package:flutter_projects/features/home/view/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      // Routing แบบง่าย
+      initialRoute: '/login',
+      routes: {
+        // '/login': (context) => const LoginPage(),
+        // '/home': (context) => const HomePage(),
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_projects/pages/HomeScreen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Homepage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
+  runApp(const ProviderScope(child: MyApp()));
 }
